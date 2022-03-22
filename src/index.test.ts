0e7dbd2c4c26f6906313bb3e0ecc7eff1da8d140
zuzanapiorkowska <napiorkowskaz@gmail.com>
2022-03-22 12:15:42 +0100

    change beforeEach place

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,35 +1,37 @@
+import { exitCode } from "process";
+import { isFloat32Array } from "util/types";
 import { TurboNumber } from "./index";
 
+
 describe(TurboNumber.name, () => {
+  let turboNumber: TurboNumber;
+beforeEach(()=> {
+  turboNumber = new TurboNumber(10);
+})
   it("divides", () => {
-    //when
-    const turboNumber = new TurboNumber(10);
-    turboNumber.divide(5);
 
     //then
-    expect(turboNumber.result()).toBe(2);
+    expect(turboNumber.divide(5).result()).toBe(2);
   });
 
   it("subtracts", () => {
+
     //when
-    const turboNumber = new TurboNumber(10);
     turboNumber.subtract(5);
 
     expect(turboNumber.result()).toBe(5);
   });
 
   it("throws error when trying to divide by 0", () => {
-    //when
-    const turboNumber = new TurboNumber(10);
-    //then
+
     expect(() => turboNumber.divide(0)).toThrow("Cannot divide by 0");
   });
 
   it("subtracts and divides", ()=> {
      //when
-     const turboNumber = new TurboNumber(17).subtract(5).divide(-3).result();
+     const result = new TurboNumber(17).subtract(5).divide(-3).result();
      //then
-     expect(turboNumber).toBe(-4);
+     expect(result).toBe(-4);
   });
 
 });
@@ -40,3 +42,18 @@ describe(TurboNumber.name, () => {
 //w JS największy iniger ma 64 bity, a nawet 65, bo jeden jest używany jako -+
 //Number.MAX_SAFE_INTIGER
 //templatki na githubie?
+
+//jest ma funkcje
+//beforeEach i afterEach - możemy je ustawiać i wewnątrz i na zewnątrz describe i to tez będzie wpływało na koljeność i może też być kilka beforeEachy i beforeAllów
+
+//git ammend
+//jak zmienić nazwę commitów?
+
+//jest coverage --coverage - branches to są drogi, jakimi może pójść kod z obsługą wyjątków. Jak 1 edge case, to 1 branch
+// branche dotyczą jakiegokolwiek isFloat32Array, switcha etc.
+
+//statement -> ast - abstract syntax tree
+
+//plugin???
+
+//gdzie beforeEach? - wewnątrz describe'a będzie lepiej zawsze, chyba że używam go w róznych describe'ach, ale pytanie czy nie musze złączyć tych describe'ów? 
