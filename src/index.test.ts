41be64f22543f8412ea316f7ccce0aea844a3631
zuzanapiorkowska <napiorkowskaz@gmail.com>
2022-03-22 12:45:33 +0100

    add TurboNumber with substract and result

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,23 @@
-describe(Turbonumber.name, () => {
+class TurboNumber {
+  givenNumber: number;
+  constructor(givenNumber: number){
+    this.givenNumber = givenNumber;
+  }
+  subtract (number: number): void {
+    this.givenNumber -= number
+  }
+
+  result(): number {
+    return this.givenNumber;
+  }
+}
+
+describe(TurboNumber.name, () => {
   it("correctly subtracts 5 from 10", () => {
     //when
-    tn = new TurboNumber(10);
-    tn.subtract(10);
-    expect(tn.result).toBe(5);
+    const tn = new TurboNumber(10);
+    tn.subtract(5);
+    //then
+    expect(tn.result()).toBe(5);
   });
 });
